9b1657dd618ed860704b9850f3c45430b060465d
remyli <rapi.remyli@gmail.com>
2017-01-24 13:03:45 +0900

    change transpile babel to buble

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,26 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import buble from 'rollup-plugin-buble';
+// import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'src/main.js',
   dest: 'dist/bundle.js',
   format: 'es',
+  sourceMap: 'inline',
   plugins: [
     nodeResolve({ jsnext: true }),  // npmモジュールを`node_modules`から
     commonjs(),  // CommonJSモジュールをES6に変換
-    babel()  // ES5に変換
+    buble({
+      target: {
+        chrome: 49,
+        node: 4,
+        firefox: 45,
+        safari: 9,
+        edge: 12,
+        ie: 11
+      }
+    })
+    // babel({ exclude: 'node_modules/**' })  // ES5に変換
   ]
 };
-
